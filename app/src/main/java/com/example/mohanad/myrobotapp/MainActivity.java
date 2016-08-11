eea373627b6f057d165f2f2f7e0636ec740167ab
mohanad86 <mohanad86@github.com>
2016-08-11 20:50:32 +0300

    Update

--- app/src/main/java/com/example/mohanad/myrobotapp/MainActivity.java
+++ app/src/main/java/com/example/mohanad/myrobotapp/MainActivity.java
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Robot Messages", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
